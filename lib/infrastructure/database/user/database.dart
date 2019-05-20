ff47d2905da592a72a1f226cb309ecf515e2a003
Hoang Kim Tuan <tuan.hoang@yedda.io>
2019-05-20 10:57:48 +0700

    fix dynamic table name

--- lib/infrastructure/database/user/database.dart
+++ lib/infrastructure/database/user/database.dart
@@ -6,6 +6,11 @@ import 'models.dart';
 abstract class UserDatabase implements DatabaseHelper<User> {}
 
 class UserDatabaseImplement extends BaseDatabaseImplement<User> implements UserDatabase {
+
+  UserDatabaseImplement() {
+    super.setTable("User");
+  }
+
   @override
   Map toMap(User object) {
     return object.toMap();
