0b27a57931b3d909ba5d046f6b5bfb1af6661be8
Hoang Kim Tuan <tuan.hoang@yedda.io>
2019-05-20 12:13:04 +0700

    cache network image

--- lib/ui/post/post_component.dart
+++ lib/ui/post/post_component.dart
@@ -4,12 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:injection/ui/post/post_presenter.dart';
 import 'package:injection/ui/post/post_view.dart';
 import 'package:injection/ui/post/post_viewmodel.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class PostScreen extends StatelessWidget with InjectorWidgetMixin {
   @override
   Widget buildWithInjector(BuildContext context, Injector injector) {
-    return Scaffold(
-        body: new PostList(new BasicPostPresenter(injector))
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new Container(
+            height: 600.0,
+            margin: EdgeInsets.only(bottom: 50),
+            child: new PostList(new BasicPostPresenter(injector)),
+          ),
+          CachedNetworkImage(
+            imageUrl: "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            placeholder: (context, url) => new CircularProgressIndicator(),
+            errorWidget: (context, url, error) => new Icon(Icons.error),
+          ),
+        ],
+      ),
     );
   }
 }
